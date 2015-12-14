57bf3733e1c937b765ad450180356a87fcc61875
firzhan <firzhan007@gmail.com>
2015-12-14 19:22:00 +0530

    updated the performance artifacts

--- camunda-workspace/loan-approval/src/main/java/org/camunda/bpm/loanapproval/LoanApprovalApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.loanapproval;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Loan Approval App")
-public class LoanApprovalApplication extends ServletProcessApplication {
-  // empty implementation
-}
